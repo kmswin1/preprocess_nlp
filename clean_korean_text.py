94eaddc666552cd1b7e9304d70d0ef25dc3ac0c5
lucas.ai <lucas.ai@kakaoenterprise.com>
2021-12-22 19:49:18 +0900

    add clean text

---
+++ clean_korean_text.py
@@ -0,0 +1,30 @@
+import re, json
+from kss import split_sentences
+import sys
+input = sys.argv[1]
+output = sys.argv[2]
+
+def clean_text(sentence):
+    text = re.sub(r'[@%\\*=()/~#&\+á?\xc3\xa1\-\|\.\:\;\!\-\,\_\~\$\'\"]', '', sentence)  # remove punctuation
+    text = re.sub(r'\d+', '', text)  # remove number
+    text = text.lower()  # lower case
+    text = re.sub(r'\s+', ' ', text) # remove extra space
+    text = re.sub(r'\[[^)]*\]','',text)  # [~]
+    text = re.sub(r'<[^>]+>','',text) #remove Html tags
+    text = re.sub(r'\s+', ' ', text) #remove spaces
+    text = re.sub(r"^\s+", '', text) #remove space from start
+    text = re.sub(r'\s+$', '', text) #remove space from the end
+    return text
+
+
+def preprocess(texts):
+    with(input, 'r') as f:
+        with open(output, 'w') as ff:
+            for line in f:
+                text = line.strip()
+                sentences = split_sentences(text)
+                for sentence in sentences:
+                    text = clean_text(sentence)
+                    ff.write(text + '\n')
+
+preprocess()
