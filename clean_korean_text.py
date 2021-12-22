2ed188aa920439f5a60d3e68716fca63dc9f4872
lucas.ai <lucas.ai@kakaoenterprise.com>
2021-12-22 20:06:38 +0900

    debugging

--- clean_korean_text.py
+++ clean_korean_text.py
@@ -1,8 +1,8 @@
 import re, json
 from kss import split_sentences
 import sys
-input = sys.argv[1]
-output = sys.argv[2]
+input = str(sys.argv[1])
+output = str(sys.argv[2])
 
 def clean_text(sentence):
     text = re.sub(r'[@%\\*=()/~#&\+á?\xc3\xa1\-\|\.\:\;\!\-\,\_\~\$\'\"]', '', sentence)  # remove punctuation
@@ -17,8 +17,8 @@ def clean_text(sentence):
     return text
 
 
-def preprocess(texts):
-    with(input, 'r') as f:
+def preprocess():
+    with open(input, 'r') as f:
         with open(output, 'w') as ff:
             for line in f:
                 text = line.strip()
